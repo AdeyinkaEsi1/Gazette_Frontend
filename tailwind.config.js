f9095a0b58258f61f81260b3a191bfe9f297cb0e
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-12-01 06:40:44 +0100

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './dist/**/*.html',  // All HTML files in the dist folder
+    './src/**/*.css',    // Any CSS files where Tailwind classes might appear
+    './dist/**/*.js',    // Any JavaScript files using Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
